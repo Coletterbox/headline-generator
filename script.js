2af0ae1d3f83e2ed43949fa1304bdec5f61c1609
Colette <colette.rs.shaw@gmail.com>
2019-08-03 08:58:50 +0100

    create arrays

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 // window.onload = alert("test");
+
+// first phrase
+
+const noun1 = [
+  "Price",
+  "Quality of life",
+  "Life expectancy",
+  "Quantity",
+  "Cost"
+];
+
+// ...of...
+
+const noun2 = [
+  "potatoes",
+  "tea",
+  "Boris Johnson",
+  "immigrants",
+  "the elderly",
+  "private schools",
+  "stay-at-home mothers"
+];
+
+const verb1 = ["plummets", "skyrockets"];
+
+const connectingPhrase = ["because of", "due to", "as a result of"];
+
+const noun3 = [
+  "Brexit",
+  "immigrants",
+  "the weather",
+  "climate change",
+  "Muslims",
+  "ethnic minorities",
+  "millenials",
+  "school children",
+  "baby boomers",
+  "the pope",
+  "state schools",
+  "single mothers",
+  "race mixing"
+];
+
+function constructPhrase() {
+  document.getElementById("headline").textContent = "test";
+}
+
+window.onload = constructPhrase();
+
+// second phrase
+
+// Brexit means...
+
+const brexit = [
+  "Brexit",
+  "breakfast",
+  "death",
+  "Boris",
+  "cancer",
+  "immigrants",
+  "apartheid"
+];
